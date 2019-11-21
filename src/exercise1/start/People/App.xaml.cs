83fecdc3b8bcb7eb76f1949689ee826698f4893c
Sibusiso Labi <skylasbu@gmail.com>
2019-11-21 16:04:36 +0200

    Store data locally using SQLite Database

--- src/exercise1/start/People/App.xaml.cs
+++ src/exercise1/start/People/App.xaml.cs
@@ -10,15 +10,16 @@ namespace People
     public partial class App : Application
     {
         string dbPath => FileAccessHelper.GetLocalFilePath("people.db3");
+        public static PersonRepository PersonRepo { get; set; }
 
         public App()
         {
             InitializeComponent();
 
-            MainPage = new People.MainPage()
-            {
-                Text = dbPath,
-            };
+            MainPage = new People.MainPage();
+
+            PersonRepo = new PersonRepository(dbPath);
+            
         }
 
         protected override void OnStart()
